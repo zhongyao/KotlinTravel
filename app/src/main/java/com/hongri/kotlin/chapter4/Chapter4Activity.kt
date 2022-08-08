0aa9c15cc3a3829c810a35f91f2da7eda0c0d023
hongri <1285052668@qq.com>
2022-08-08 23:19:36 +0800

    Kotlin中使用Context

---
+++ app/src/main/java/com/hongri/kotlin/chapter4/Chapter4Activity.kt
@@ -0,0 +1,38 @@
+package com.hongri.kotlin.chapter4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.hongri.kotlin.R
+import kotlinx.android.synthetic.main.activity_chapter4.*
+
+class Chapter4Activity : AppCompatActivity(), View.OnClickListener {
+    val instance by lazy { this }//这里使用了委托，表示只有使用到instance才会执行该段代码
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chapter4)
+        btnAlertDialog.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btnAlertDialog -> {
+                AlertDialog.Builder(this).apply {
+                    setTitle("This is Dialog")
+                    setMessage("Something important.")
+                    setCancelable(false)
+                    setPositiveButton("OK") { dialog, which ->
+                        Toast.makeText(this@Chapter4Activity, "点击了OK", Toast.LENGTH_LONG).show()
+                    }
+
+                    setNegativeButton("Cancel") { dialog, which ->
+                        Toast.makeText(instance, "点击了Cancel", Toast.LENGTH_LONG).show()
+                    }
+                    show()
+                }
+            }
+        }
+    }
+}
