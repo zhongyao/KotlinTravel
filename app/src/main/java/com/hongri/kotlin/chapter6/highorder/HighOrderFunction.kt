2901b98c0dc3adff7526cd7fed5b4f8f29cb8668
hongri <1285052668@qq.com>
2022-08-12 16:56:14 +0800

    内联函数 inline oninline crossinline

--- app/src/main/java/com/hongri/kotlin/chapter6/highorder/HighOrderFunction.kt
+++ app/src/main/java/com/hongri/kotlin/chapter6/highorder/HighOrderFunction.kt
@@ -10,9 +10,32 @@ package com.hongri.kotlin.chapter6.highorder
  * 高阶函数：
  * 如果一个函数接收另一个函数作为参数，或者返回值的类型是另一个函数，那么该函数就称为高阶函数。
  */
-fun num1AndNum2(num1: Int, num2: Int, operation: (Int, Int) -> Int): Int {
+//fun num1AndNum2(num1: Int, num2: Int, operation: (Int, Int) -> Int): Int {
+//    val result = operation(num1, num2)
+//    return result
+//}
+
+/**
+ * 内联函数【inline】：内联函数的优势是不存在运行时开销
+ */
+inline fun num1AndNum2(num1: Int, num2: Int, operation: (Int, Int) -> Int): Int {
     val result = operation(num1, num2)
     return result
+}
+
+/**
+ * noinline关键字 表示不会对引用的Lambda表达式进行内联
+ *
+ * 注：
+ * 非内联的函数类型参数可以自由地传递给其他任何函数，因为他就是一个真实的参数；
+ * 而内联的函数类型参数只允许传递给另一个内联函数，这也是它最大的局限性。
+ *
+ * 内联函数和非内联函数还有一个重要的区别，那就是内联函数所引用的Lambda表达式中是可以
+ * 使用return关键字来进行函数返回的，而非内联函数只能进行局部返回。
+ *
+ */
+inline fun inlineTest(block1: () -> Unit, noinline block2: () -> Unit) {
+
 }
 
 
@@ -44,5 +67,18 @@ fun main1() {
     val result2 = num1AndNum2(num1, num2) {n1, n2 -> n1 - n2}
     println("result1 is $result1")
     println("result2 is $result2")
+}
+
+/**
+ * crossinline 关键字就像一个契约，它用于保证在内联函数的Lambda表达式中一定不会用到return关键字。
+ * 但是仍然可以使用return@runRunnable的写法进行局部返回。
+ * 总体来说，除了在return关键字的使用上有所区别外，crossinline保留了内联函数的其他所有特性
+ */
+inline fun runRunnable(crossinline block: () -> Unit) {
+    val runnable = Runnable {
+        block()
+    }
+
+    runnable.run()
 }
 
