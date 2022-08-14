0c4e51d89cbaef75951018b38cc2c23abf160207
hongri <1285052668@qq.com>
2022-08-14 08:54:29 +0800

    ContentValues推荐用法

--- app/src/main/java/com/hongri/kotlin/chapter7/Chapter7Activity.kt
+++ app/src/main/java/com/hongri/kotlin/chapter7/Chapter7Activity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.core.content.contentValuesOf
+import androidx.core.content.edit
 import com.hongri.kotlin.R
 import kotlinx.android.synthetic.main.activity_chapter7.*
 import java.io.*
@@ -58,11 +60,27 @@ class Chapter7Activity : AppCompatActivity() {
         }
 
         saveSpBtn.setOnClickListener {
+            //方式1
             val editor = getSharedPreferences("data", Context.MODE_PRIVATE).edit()
             editor.putString("name", "yao")
             editor.putString("gender", "male")
             editor.putBoolean("goodman", true)
             editor.apply()
+
+            //方式2
+            getSharedPreferences("data", Context.MODE_PRIVATE).edit().apply {
+                putString("name2", "yao2")
+                putString("gender", "male")
+                putBoolean("goodman", true)
+                apply()
+            }
+
+            //方式3
+            getSharedPreferences("data", Context.MODE_PRIVATE).edit {
+                putString("name3", "yao3")
+                putString("gender", "male")
+                putBoolean("goodman", true)
+            }
         }
 
         restoreSpBtn.setOnClickListener {
@@ -175,6 +193,78 @@ class Chapter7Activity : AppCompatActivity() {
             //升级到第3版本时这样写
             val db = MyDatabaseHelper(this, "BookStore.db", 3)
             db.writableDatabase
+        }
+
+        cvBetterBtn.setOnClickListener {
+            val db = dbHelper.writableDatabase
+
+            //用法1
+//            val values = ContentValues()
+//            values.put("name", "yao book")
+//            values.put("author", "yao")
+//            values.put("pages", 334)
+//            values.put("price", 39.9)
+//            db.insert("Book", null, values)
+
+            //用法2
+//            val values = ContentValues().apply {
+//                put("name", "yao book")
+//                put("author", "yao")
+//                put("pages", 334)
+//                put("price", 39.9)
+//            }
+//            db.insert("Book", null, values)
+
+
+            //用法3
+//            val values = cvOf("name" to "yao book", "author" to "yao", "pages" to 334, "price" to 39.9)
+//            db.insert("Book", null, values)
+
+            //用法4
+//            val values = cvOf2("name" to "yao book", "author" to "yao", "pages" to 334, "price" to 39.9)
+//            db.insert("Book", null, values)
+
+            //用法5【推荐】
+            val values = contentValuesOf("name" to "yao book", "author" to "yao", "pages" to 334, "price" to 39.9)
+            db.insert("Book", null, values)
+        }
+    }
+
+    fun cvOf(vararg pairs: Pair<String, Any?>): ContentValues {
+        val cv = ContentValues()
+        for (pair in pairs) {
+            val key = pair.first
+            when (val value = pair.second) {
+                is Int -> cv.put(key, value)
+                is Long -> cv.put(key, value)
+                is Short -> cv.put(key, value)
+                is Float -> cv.put(key, value)
+                is Double -> cv.put(key, value)
+                is Boolean -> cv.put(key, value)
+                is String -> cv.put(key, value)
+                is Byte -> cv.put(key, value)
+                is ByteArray -> cv.put(key, value)
+                null -> cv.putNull(key)
+            }
+        }
+        return cv
+    }
+
+    fun cvOf2(vararg pairs: Pair<String, Any?>) = ContentValues().apply {
+        for (pair in pairs) {
+            val key = pair.first
+            when (val value = pair.second) {
+                is Int -> put(key, value)
+                is Long -> put(key, value)
+                is Short -> put(key, value)
+                is Float -> put(key, value)
+                is Double -> put(key, value)
+                is Boolean -> put(key, value)
+                is String -> put(key, value)
+                is Byte -> put(key, value)
+                is ByteArray -> put(key, value)
+                null -> putNull(key)
+            }
         }
     }
 }
