21ba9f27fd1548c3422fc385ae16df82d7843fb5
hongri <1285052668@qq.com>
2022-08-23 22:04:25 +0800

    绑定停止Service

--- app/src/main/java/com/hongri/kotlin/chapter10/Chapter10Activity.kt
+++ app/src/main/java/com/hongri/kotlin/chapter10/Chapter10Activity.kt
@@ -1,10 +1,14 @@
 package com.hongri.kotlin.chapter10
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.IBinder
 import android.os.Message
+import android.util.Log
 import com.hongri.kotlin.R
 import kotlinx.android.synthetic.main.activity_chapter10.*
 import kotlin.concurrent.thread
@@ -12,13 +16,27 @@ import kotlin.concurrent.thread
 class Chapter10Activity : AppCompatActivity() {
 
     private val updateText = 1;
-
+    private val tag = "Chapter10Activity";
     private val handler = object : Handler() {
         override fun handleMessage(msg: Message) {
             super.handleMessage(msg)
-            when(msg.what) {
+            when (msg.what) {
                 updateText -> tvContent.text = "Nice to meet you."
             }
+        }
+    }
+
+    lateinit var downloadBinder: MyService.DownloadBinder
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            Log.d(tag, "onServiceConnected")
+            downloadBinder = service as MyService.DownloadBinder
+            downloadBinder.startDownload()
+            downloadBinder.getProgress()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            Log.d(tag, "onServiceDisconnected")
         }
     }
 
@@ -51,5 +69,15 @@ class Chapter10Activity : AppCompatActivity() {
             val intent = Intent(this, MyService::class.java)
             stopService(intent)
         }
+
+        bindServiceBtn.setOnClickListener {
+            val intent = Intent(this, MyService::class.java)
+            this.bindService(intent, connection, BIND_AUTO_CREATE)
+        }
+
+        unBindServiceBtn.setOnClickListener {
+            unbindService(connection)
+        }
+
     }
 }
--- app/src/main/java/com/hongri/kotlin/chapter10/MyService.kt
+++ app/src/main/java/com/hongri/kotlin/chapter10/MyService.kt
@@ -2,14 +2,29 @@ package com.hongri.kotlin.chapter10
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
 import android.util.Log
 
 class MyService : Service() {
 
     private val TAG = "MyService"
+    private val mBinder = DownloadBinder()
+
+    class DownloadBinder : Binder() {
+        fun startDownload() {
+            Log.d("MyService", "startDownload")
+        }
+
+        fun getProgress(): Int {
+            Log.d("MyService", "getProgress")
+            return 0
+        }
+    }
+
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        Log.d(TAG, "onBind")
+        return mBinder
     }
 
     override fun onCreate() {
