17aa303792c6bcdb884e2258e47a5636369bfa0f
hongri <1285052668@qq.com>
2022-08-30 17:49:05 +0800

    协程

--- app/src/main/java/com/hongri/kotlin/chapter11/Chapter11Activity.kt
+++ app/src/main/java/com/hongri/kotlin/chapter11/Chapter11Activity.kt
@@ -8,6 +8,7 @@ import com.hongri.kotlin.chapter11.retrofit.App
 import com.hongri.kotlin.chapter11.retrofit.AppService
 import com.hongri.kotlin.chapter11.retrofit.ServiceCreator
 import kotlinx.android.synthetic.main.activity_chapter11.*
+import kotlinx.coroutines.*
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import retrofit2.Call
@@ -49,6 +50,77 @@ class Chapter11Activity : AppCompatActivity() {
                 }
 
             })
+        }
+
+        coroutinesBtn.setOnClickListener {
+            /**
+             * runBlocking函数同样会创建一个协程的作用域，但是它可以
+             * 保证在协程作用域内的所有代码和子协程没有全部执行完之前一直阻塞当前线程。
+             * 【需要注意的是runBlocking容易产生一些性能上的问题】
+             */
+//            runBlocking {
+//                Log.d(TAG, "codes run in coroutine scope")
+//                delay(1500)
+//                Log.d(TAG, "codes run in coroutine scope finished")
+//            }
+
+            /**
+             * 使用launch函数创建多个协程
+             * 子协程的特点是如果外层作用域的协程结束了，那么该作用域下的所有子协程都会结束。
+             */
+/*            runBlocking {
+                launch {
+                    Log.d(TAG, "launch-1")
+                    delay(1000)
+                    Log.d(TAG, "launch-1 finished")
+                }
+
+                launch {
+                    Log.d(TAG, "launch-2")
+                    delay(1000)
+                    Log.d(TAG, "launch-2 finished")
+                }
+            }*/
+
+            /**
+             * 验证协程的高效率
+             */
+//            val start = System.currentTimeMillis()
+//            runBlocking {
+//                repeat(100000) {
+//                    launch {
+//                        Log.d(TAG, ".")
+//                    }
+//                }
+//            }
+//            val end = System.currentTimeMillis()
+//            Log.d(TAG, ((end - start).toString()))
+
+            /**
+             * 调用挂起函数
+             */
+            val start = System.currentTimeMillis()
+            runBlocking {
+                repeat(100000) {
+                    printDot()
+                }
+            }
+            val end = System.currentTimeMillis()
+            Log.d(TAG, ((end - start).toString()))
+        }
+    }
+
+    /**
+     * 调用挂起函数
+     * coroutineScope跟runBlocking函数区别：
+     * 1、coroutineScope只会阻塞当前协程，既不影响其他协程，也不影响任何线程，因此不会造成性能上的问题
+     * 2、runBlocking由于会阻塞当前线程，如果再主线程中调用的话，可能会造成页面卡死。
+     */
+    private suspend fun printDot() = coroutineScope {
+        launch {
+            Log.d(TAG, ".")
+            //挂起函数
+            delay(1000)
         }
     }
 
